16c74544a93eabcff231da6910258d01ca0cee2b
nehaApurohit <neha.a.purohit@accenture.com>
2020-11-25 13:54:32 -0500

    first commit

---
+++ src/SortClass.java
@@ -0,0 +1,187 @@
+import java.util.*;
+
+public class SortClass {
+
+    public static ArrayList<Integer> selectionSort(ArrayList<Integer> array) {
+        for (int i = 0; i < array.size() - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.size(); j++)
+                if (array.get(j) < array.get(min))
+                    min = j;
+
+            int temp = array.get(min);
+            array.set(min, array.get(i));
+            array.set(i, temp);
+        }
+        return array;
+    }
+
+    public static ArrayList<Integer> bubbleSort(ArrayList<Integer> array) {
+        for (int j = 0; j < array.size(); j++) {
+            for (int i = 1; i < array.size(); i++) {
+                if (array.get(i) < array.get(i - 1)) {
+                    int temp = array.get(i - 1);
+                    array.set(i - 1, array.get(i));
+                    array.set(i, temp);
+                }
+            }
+        }
+        return array;
+    }
+
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> array, int size) {
+        if (size <= 1) return array;
+        insertionSort(array, size - 1);
+        int temp = array.get(size - 1);
+        int j = size - 2;
+        while (j >= 0 && array.get(j) > temp) {
+            array.set(j + 1, array.get(j));
+            j--;
+        }
+        array.set(j + 1, temp);
+
+        return array;
+    }
+
+    public static ArrayList<Integer> mergeSort(ArrayList<Integer> array) {
+        if (array.size() <= 1) return array;
+        int mid = array.size() / 2;
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+        for (int i = 0; i < mid; i++) {
+            left.add(i, array.get(i));
+        }
+        for (int i = 0; i < array.size() - mid; i++) {
+            right.add(i, array.get(mid + i));
+        }
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
+    }
+
+    private static ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right) {
+        ArrayList<Integer> aux = new ArrayList<>();
+        int leftPoiner, mergePointer, rightPointer;
+        leftPoiner = rightPointer = mergePointer = 0;
+
+        while (left.size() > leftPoiner && right.size() > rightPointer) {
+            if (left.get(leftPoiner) <= right.get(rightPointer)) {
+                aux.add(mergePointer, left.get(leftPoiner));
+                leftPoiner++;
+                mergePointer++;
+            } else {
+                aux.add(mergePointer, right.get(rightPointer));
+                rightPointer++;
+                mergePointer++;
+            }
+        }
+        while (left.size() > leftPoiner) {
+            aux.add(mergePointer, left.get(leftPoiner));
+            mergePointer++;
+            leftPoiner++;
+        }
+        while (right.size() > rightPointer) {
+            aux.add(mergePointer, right.get(rightPointer));
+            mergePointer++;
+            rightPointer++;
+        }
+
+        return aux;
+    }
+
+    public static ArrayList<Integer> quickSort(ArrayList<Integer> array, int start, int end) {
+        if (start >= end) return array;
+        Random rand = new Random();
+
+        int pivotInd = rand.nextInt((end - start) + 1) + start;
+        int smallInd = 0;
+        int pivot = array.get(pivotInd);
+        array.set(pivotInd, array.get(smallInd));
+        array.set(smallInd, pivot);
+        smallInd++;
+        for (int i = 1; i < array.size(); i++) {
+            if (array.get(i) < pivot) {
+                int temp = array.get(smallInd);
+                array.set(smallInd, array.get(i));
+                array.set(i, temp);
+                smallInd++;
+            }
+
+        }
+        array.set(start, array.get(smallInd - 1));
+        array.set(smallInd - 1, pivot);
+        quickSort(array, start, smallInd - 1);
+        quickSort(array, smallInd + 1, end);
+        return array;
+    }
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList();
+        list.add(1);
+        list.add(4);
+        list.add(3);
+        list.add(6);
+        list.add(7);
+        list.add(9);
+        list.add(10);
+        list.add(12);
+
+        //  System.out.println(selectionSort(list));
+        //   System.out.println(bubbleSort(list));
+//          System.out.println(insertionSort(list,list.size()));
+        //  System.out.println(quickSort(list,0,list.size()-1));
+        System.out.println(heapSort(list));
+
+
+    }
+    static List resultHeap = new ArrayList();
+    private static List heapSort(ArrayList<Integer> list) {
+        list = heappifyDown(list);
+        list = heappifyUp(list);
+
+      int max = list.get(0);
+        resultHeap.add(max);
+        while (list.size()>1) {
+            list.remove(0);
+            heapSort(list);
+        }
+      return resultHeap;
+    }
+
+    private static ArrayList<Integer> heappifyDown(ArrayList<Integer> list) {
+        for (int i = 0; i < (list.size()) / 2; i++) {
+            int max= finMaxOfThree(list.get(2 * i),list.get(i),list.get(2 * i + 1));
+            if (list.get(i) == max) continue;
+            else {
+                int temp = list.get(i);
+                if (list.get(2 * i) == max) {
+                    list.set(i, list.get(2 * i));
+                    list.set((2 * i), temp);
+                } else {
+                    list.set(i, list.get(2 * i + 1));
+                    list.set((2 * i + 1), temp);
+                }
+            }
+        }
+        return list;
+    }
+    private static ArrayList<Integer> heappifyUp(ArrayList<Integer> list) {
+        for (int i = (list.size())-1; i > 0; i--) {
+            if (list.get(i)<=list.get(i/2)) continue;
+            else {
+                int temp = list.get(i);
+                list.set(i, list.get(i/2));
+                list.set((i/2), temp);
+            }
+        }
+        return list;
+    }
+
+    private static int finMaxOfThree(int num1, int num2, int num3) {
+        if (num1 >= num2 && num1 >= num3) return num1;
+        else if (num2 >= num1 && num2 >= num3) return num2;
+        else return num3;
+        }
+
+}
