397aecc12618d56407530cb96ced9267034a9fc6
nehaApurohit <neha.a.purohit@accenture.com>
2021-02-07 20:49:30 -0500

    More DP Problems Solved

---
+++ src/dp/WordBreakDP.java
@@ -0,0 +1,37 @@
+package dp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WordBreakDP {
+
+    public static boolean wordBreak(String s, List<String> wordDict) {
+
+        char[] chars = s.toCharArray();
+        boolean[] table = new boolean[chars.length+1];
+
+        Arrays.fill(table,false);
+        //base case
+        table[0] = true;
+        if (wordDict.contains(s))table[chars.length]=true;
+        //recursive case
+        for (int i=1;i<chars.length+1;i++){
+            //j iterate over substring of all length from 0 to i
+            for (int j = 0; j < i; j++) {
+                if(wordDict.contains(s.substring(j,i)) && table[j]){
+                    table[i]= true;
+                }
+            }
+
+        }
+        return table[chars.length];
+    }
+
+    public static void main(String[] args) {
+        List<String> dictionary = new ArrayList<>();
+        dictionary.add("apple");
+        dictionary.add("pen");
+        System.out.println(wordBreak("applepenapple",dictionary));
+    }
+}
